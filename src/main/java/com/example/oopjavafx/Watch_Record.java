0b240f0da9cb9a9add7d2e103e8be4e7f38b1d83
Ali Amgad <129622410+aliamgad@users.noreply.github.com>
2023-12-20 19:45:13 +0200

    Update Watch_Record to latest version

--- src/main/java/com/example/oopjavafx/Watch_Record.java
+++ src/main/java/com/example/oopjavafx/Watch_Record.java
@@ -1,6 +1,8 @@
 package com.example.oopjavafx;
 
-public class Watch_Record{
+import java.util.ArrayList;
+
+public class Watch_Record {
 
     private final int userId;
     private Movie movie;
@@ -12,7 +14,8 @@ public class Watch_Record{
     public Watch_Record(int userId) {
         this.userId = userId;
     }
-    public Watch_Record(int userId,Movie movie,int rating,int day,int month,int year) {
+
+    public Watch_Record(int userId, Movie movie, int rating, int day, int month, int year) {
         this.userId = userId;
         this.movie = movie;
         this.rating = rating;
@@ -38,21 +41,62 @@ public class Watch_Record{
     }
 
     public void setRating(Integer rating) {
-        if(rating ==null||rating>=0 &&rating<5) {
+        if (rating == null || rating >= 0 && rating < 5) {
             this.rating = rating;
         }
     }
 
 
-
-
     public void getDate() {
-        System.out.println(day+"/"+month+ "/"+year);
-    }
-    public void setDate(int d,int m,int y) {
-        day=d;
-        month=m;
-        year=y;
+        System.out.println(day + "/" + month + "/" + year);
     }
 
+    public void setDate(int d, int m, int y) {
+        day = d;
+        month = m;
+        year = y;
+    }
+
+    void showThisWatchRecordDetails(Watch_Record watchRecord) {
+        System.out.println("User ID: " + watchRecord.userId);
+        System.out.println("Movie Name: " + watchRecord.movie);
+        System.out.println("Date: " + day + "/" + month + "/" + year);
+        System.out.println("Movie Rating : " + watchRecord.rating);
+    }
+
+    void getLastWatchRecord(ArrayList<Watch_Record> watchRecords) {
+        if (!watchRecords.isEmpty()) {
+            int size_w = watchRecords.size() - 1;
+            System.out.println(watchRecords.get(size_w));
+        } else {
+            System.out.println("Watch Record is Empty!");
+        }
+    }
+
+    void getAllWatchRecord(ArrayList<Watch_Record> watchRecords) {
+        if (!watchRecords.isEmpty()) {
+
+            for (Watch_Record wr : watchRecords) {
+                showThisWatchRecordDetails(wr);
+            }
+        } else {
+            System.out.println("Watch Record is Empty!");
+        }
+    }
+
+    void deletelastRecord(ArrayList<Watch_Record> watchRecords) {
+        if (!watchRecords.isEmpty()) {
+            int size_w = watchRecords.size() - 1;
+            watchRecords.remove(size_w);
+        } else System.out.println("Records are Already Empty!");
+    }
+
+    void deleteAllRecords(ArrayList<Watch_Record> watchRecords) {
+        if (!watchRecords.isEmpty()) {
+            int size_w = watchRecords.size() - 1;
+            for (int i = size_w - 1; i >= 0; i--) {
+                deletelastRecord(watchRecords);
+            }
+        } else System.out.println("Records are Already Empty!");
+    }
 }
