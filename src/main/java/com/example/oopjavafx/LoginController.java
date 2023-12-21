f4643d4ffc41275d5fbdd54269a066ae61e94cc8
Cellz <54197394+Cellzawy@users.noreply.github.com>
2023-12-21 04:32:25 +0200

    User files done

--- src/main/java/com/example/oopjavafx/LoginController.java
+++ src/main/java/com/example/oopjavafx/LoginController.java
@@ -15,17 +15,19 @@ import javafx.stage.Stage;
 
 import java.io.*;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.regex.Pattern;
 
-
 public class LoginController implements Initializable {
 
-    public boolean checkInputs (String email, String password) {
-        return  Pattern.compile("^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$").matcher(email).find()
+    public boolean checkInputs(String email, String password) {
+        return Pattern.compile("^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$").matcher(email).find()
                 &&
                 Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,20}$").matcher(password).find();
     }
+
     private Scene Register;
 
     private Scene scene;
@@ -33,7 +35,7 @@ public class LoginController implements Initializable {
     private Parent root;
 
     public void ChangeScene(ActionEvent event) {
-        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
@@ -57,37 +59,78 @@ public class LoginController implements Initializable {
         stage.setHeight(818);
     }
 
-    public void Login(ActionEvent event) throws Exception{
-        if(!checkInputs(email.getText(), password.getText())) {
+    public void Login(ActionEvent event) throws Exception {
+        if (!checkInputs(email.getText(), password.getText())) {
             errorText.setTextFill(Color.RED);
             errorText.setText("Invalid inputs !! Try again");
         }
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\alyas\\IdeaProjects\\OOPJavaFX\\src\\main\\resources\\Files\\" + email.getText() +".txt"));
-                user.setFirst_name(reader.readLine());
-                user.setLast_name(reader.readLine());
-                user.setUser_email(reader.readLine());
-                user.setUser_password(reader.readLine());
-                reader.close();
-            if(user.getUser_password().toString().equals(password.getText())){
-                errorText.setTextFill(Color.valueOf("#4dde90"));
-                errorText.setText("Login Successfully!");
-                root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
-                ChangeScene(event);
-                stage.setResizable(true);
-                stage.setWidth(1920);
-                stage.setHeight(1080);
-                stage.setY(0);
-                stage.setX(0);
-//                Button newButton = new Button();
-//                root.
+            String readLine = "";
+            FileReader reader = new FileReader("src\\main\\resources\\Files\\users.txt");
+            int c;
+            while ((c = reader.read()) != -1) {
+                readLine += (char) c;
             }
-            else {
+            reader.close();
+            String fnameReadText = "", lnameReadText = "", emailReadText = "", pwReadText = "";
+            String userStringData[] = { fnameReadText, lnameReadText, emailReadText, pwReadText };
+            ArrayList<User> userData = new ArrayList<User>();
+            int dataIndex = 0;
+            for (int i = 0; i < readLine.length(); i++) {
+                int length = 0;
+                userStringData[dataIndex] = "";
+                String lengthSubsStr = readLine.substring(i, readLine.indexOf('#', i));
+                System.out.println("STRING: " + lengthSubsStr);
+                System.out.println(i);
+
+                length = Integer.parseInt(lengthSubsStr);
+                System.out.println(length);
+                i = readLine.indexOf('#', i);
+                for (int j = 1; j < length + 1; j++) {
+                    userStringData[dataIndex] += readLine.charAt(i + j);
+                }
+                i += userStringData[dataIndex].length();
+                System.out.println(userStringData[dataIndex]); // For testing
+                dataIndex++;
+                if (dataIndex == 4) {
+                    dataIndex = 0;
+                    User u = new User();
+                    u.setFirst_name(userStringData[0]);
+                    u.setLast_name(userStringData[1]);
+                    u.setUser_email(userStringData[2]);
+                    u.setUser_password(userStringData[3]);
+                    userData.add(u);
+                    System.out.println( // For testing
+                            "User: " + u.getFirst_name() + " " + u.getLast_name() + " " + u.getUser_email() + " "
+                                    + u.getUser_password());
+                }
+            }
+            boolean found = false;
+            for (User u : userData) {
+                if (u.getUser_password().equals(password.getText()) && u.getUser_email().equals(email.getText())) {
+                    found = true;
+                    errorText.setTextFill(Color.valueOf("#4dde90"));
+                    errorText.setText("Login Successfully!");
+                    root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+                    ChangeScene(event);
+                    stage.setResizable(true);
+                    stage.setWidth(1920);
+                    stage.setHeight(1080);
+                    stage.setY(0);
+                    stage.setX(0);
+                    // Button newButton = new Button();
+                    // root.
+                } else if (found == true) {
+                    break;
+                }
+            }
+            if (!found) {
                 errorText.setTextFill(Color.RED);
-                errorText.setText("Wrong Password!");
+                errorText.setText("User or password does not match!");
             }
         } catch (Exception e) {
-            errorText.setText("User is not Found !");
+            // errorText.setText(e.getMessage());
+            System.out.println(e.getMessage());
         }
     }
 
--- src/main/java/com/example/oopjavafx/RegisterController.java
+++ src/main/java/com/example/oopjavafx/RegisterController.java
@@ -14,13 +14,14 @@ import javafx.stage.Stage;
 
 import java.io.*;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import java.util.regex.Pattern;
 
 public class RegisterController implements Initializable {
 
-    public boolean checkInputs (String email, String password) {
-        return  Pattern.compile("^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$").matcher(email).find()
+    public boolean checkInputs(String email, String password) {
+        return Pattern.compile("^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$").matcher(email).find()
                 &&
                 Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,20}$").matcher(password).find();
     }
@@ -29,17 +30,16 @@ public class RegisterController implements Initializable {
         String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "abcdefghijklmnopqrstuvxyz";
         StringBuilder s = new StringBuilder(6);
         for (int i = 0; i < 6; i++) {
-            int index = (int)(AlphaNumericString.length() * Math.random());
+            int index = (int) (AlphaNumericString.length() * Math.random());
             s.append(AlphaNumericString.charAt(index));
         }
         return s.toString();
     }
 
-
     Thread countdown = new Thread(new Runnable() {
         @Override
         public void run() {
-            for(int i = 2 ; i> 0 ; i--) {
+            for (int i = 2; i > 0; i--) {
                 System.out.println();
                 try {
                     Thread.sleep(1000);
@@ -51,7 +51,7 @@ public class RegisterController implements Initializable {
     });
 
     public void ChangeScene(ActionEvent event) {
-        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene1 = new Scene(root);
         stage.setScene(scene1);
         stage.setResizable(false);
@@ -80,6 +80,7 @@ public class RegisterController implements Initializable {
     Label RandomCode;
     @FXML
     TextField Code;
+
     public void ChangetoLogin(ActionEvent event) throws Exception {
         root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         ChangeScene(event);
@@ -90,31 +91,82 @@ public class RegisterController implements Initializable {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
         root = loader.load();
         LoginController loginController = loader.getController();
-        if(!checkInputs(email.getText(), password.getText())) {
-            errorText.setText("Incorrect Format for Password or Email !");
-                countdown.start();
-                while(countdown.isAlive()) {}
-        } else if (!confirmedPassword.getText().equals(password.getText())){
-            errorText.setText("Passwords doesn't match !");
+        if (!checkInputs(email.getText(), password.getText())) {
+            errorText.setText("Incorrect format for password or email!");
+            countdown.start();
+            while (countdown.isAlive()) {
+            }
+        } else if (!confirmedPassword.getText().equals(password.getText())) {
+            errorText.setText("Passwords doesn't match!");
         } else if (!Code.getText().equals(RandomCode.getText())) {
-            errorText.setText("Incorrect Code !");
+            errorText.setText("Incorrect Code!");
         } else {
             try {
-                BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\alyas\\IdeaProjects\\OOPJavaFX\\src\\main\\resources\\Files\\" + email.getText() +".txt"));
-                System.out.println(reader.readLine());
-                System.out.println(reader.readLine());
-                if(email.getText().equals(reader.readLine().toString())) {
-                    errorText.setText("User already Registered !");
-                    return;
+                String readLine = "";
+                FileReader reader = new FileReader("src\\main\\resources\\Files\\users.txt");
+                int c;
+                while ((c = reader.read()) != -1) {
+                    readLine += (char) c;
+                }
+                reader.close();
+                String fnameReadText = "", lnameReadText = "", emailReadText = "", pwReadText = "";
+                String userStringData[] = { fnameReadText, lnameReadText, emailReadText, pwReadText };
+                ArrayList<User> userData = new ArrayList<User>();
+                int dataIndex = 0;
+                for (int i = 0; i < readLine.length(); i++) {
+                    int length = 0;
+                    userStringData[dataIndex] = "";
+                    String lengthSubsStr = readLine.substring(i, readLine.indexOf('#', i));
+                    System.out.println("STRING: " + lengthSubsStr);
+                    System.out.println(i);
+
+                    length = Integer.parseInt(lengthSubsStr);
+                    System.out.println(length);
+                    i = readLine.indexOf('#', i);
+                    for (int j = 1; j < length + 1; j++) {
+                        userStringData[dataIndex] += readLine.charAt(i + j);
+                    }
+                    i += userStringData[dataIndex].length();
+                    System.out.println(userStringData[dataIndex]); // For testing
+                    dataIndex++;
+                    if (dataIndex == 4) {
+                        dataIndex = 0;
+                        User u = new User();
+                        u.setFirst_name(userStringData[0]);
+                        u.setLast_name(userStringData[1]);
+                        u.setUser_email(userStringData[2]);
+                        u.setUser_password(userStringData[3]);
+                        userData.add(u);
+                        System.out.println( // For testing
+                                "User: " + u.getFirst_name() + " " + u.getLast_name() + " " + u.getUser_email() + " "
+                                        + u.getUser_password());
+                    }
+                }
+                boolean found = false;
+                for (User u : userData) {
+                    if (email.getText().equals(u.getUser_email())) {
+                        errorText.setText("User already registered!");
+                        return;
+                    } else if (found == true) {
+                        break;
+                    }
                 }
             } catch (Exception e) {
             }
             try {
-                BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\alyas\\IdeaProjects\\OOPJavaFX\\src\\main\\resources\\Files\\" + email.getText() +".txt"));
-                writer.write(f_name.getText()+"\n");
-                writer.write(l_name.getText()+"\n");
-                writer.write(email.getText()+"\n");
-                writer.write(password.getText()+"\n");
+                String readLine = "";
+                FileReader reader = new FileReader("src\\main\\resources\\Files\\users.txt");
+                int c;
+                while ((c = reader.read()) != -1) {
+                    readLine += (char) c;
+                }
+                reader.close();
+                FileWriter writer = new FileWriter("src\\main\\resources\\Files\\users.txt");
+                writer.write(f_name.getText().length() + "#" + f_name.getText());
+                writer.write(l_name.getText().length() + "#" + l_name.getText());
+                writer.write(email.getText().length() + "#" + email.getText());
+                writer.write(password.getText().length() + "#" + password.getText());
+                writer.write(readLine);
                 writer.close();
             } catch (Exception e) {
                 return;
@@ -124,6 +176,7 @@ public class RegisterController implements Initializable {
             loginController.errorText.setText("Registered Successfully !");
             ChangeScene(event);
         }
+
     }
 
     @Override
